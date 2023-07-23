8bc58d20f5ba6b4b42a9d4b1470a471e401be37b
quentinkrammer <bopatzable@gmail.com>
2023-07-23 16:11:45 +0200

    stuff: mostly lint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,24 @@
 module.exports = {
-  extends: ["turbo", "prettier"],
-  rules: { },
+  extends: [
+    "turbo",
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react-hooks/recommended",
+  ],
+  env: { browser: true, es2020: true },
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-   
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/no-non-null-assertion": "off",
   },
 };
