83e0fdf3183d4405e2dbe7a99f74660e10fe11e7
quentinkrammer <bopatzable@gmail.com>
2023-08-05 13:45:21 +0200

    WIP backend

---
+++ app1/src/trpcClient.ts
@@ -0,0 +1,12 @@
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import type { AppRouter } from "backend1";
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+export const trpc = createTRPCProxyClient<AppRouter>({
+  links: [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    httpBatchLink({
+      url: "http://localhost:3000",
+    }),
+  ],
+});
---
+++ backend1/src/index.ts
@@ -0,0 +1,36 @@
+import { createHTTPServer } from "@trpc/server/adapters/standalone";
+import { v4 as uuidv4 } from "uuid";
+import { z } from "zod";
+import { publicProcedure, router } from "./trpc.js";
+type User = { id: string; name: string };
+const db: Array<User> = [];
+
+const appRouter = router({
+  userList: publicProcedure.query(async () => {
+    return db;
+  }),
+  userById: publicProcedure.input(z.string()).query(async (opts) => {
+    const { input: searchId } = opts;
+
+    // Retrieve the user with the given ID
+    const user = db.find(({ id }) => id === searchId);
+
+    return user;
+  }),
+  userCreate: publicProcedure.input(z.string()).mutation(async (opts) => {
+    const { input: name } = opts;
+
+    // Create a new user in the database
+    const user = db.push({ name, id: uuidv4() });
+
+    return user;
+  }),
+});
+
+export type AppRouter = typeof appRouter;
+
+const server = createHTTPServer({
+  router: appRouter,
+});
+
+server.listen(3000);
