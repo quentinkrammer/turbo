dd26fe832e55ee91122f3084750c934bfb36633d
quentinkrammer <bopatzable@gmail.com>
2023-08-06 14:54:54 +0200

    create TrpcAndReduxTestTable

---
+++ app1/src/components/TrpcAndReduxTestTable.tsx
@@ -0,0 +1,40 @@
+import { useCallback, useEffect, useState } from "react";
+import { CustomTable } from "shared";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { selectTableData, sortName } from "../redux/tableSlice";
+import { trpc } from "../trpcClient";
+
+export function TrpcAndReduxTestTable() {
+  const dispatch = useAppDispatch();
+  const data = useAppSelector(selectTableData);
+  const [users, setUsers] = useState<
+    Awaited<ReturnType<(typeof trpc)["userList"]["query"]>>
+  >([]);
+  const [mutatedUsers, setMutatedUsers] = useState(false);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const users = await trpc.userList.query();
+      setUsers(users);
+    };
+    fetchUsers().catch(console.error);
+  }, [mutatedUsers]);
+
+  const onCreateUser = useCallback(async () => {
+    await trpc.userCreate.mutate("Jerry");
+    setMutatedUsers((old) => !old);
+  }, []);
+
+  return (
+    <>
+      <button type="button" onClick={() => void onCreateUser()}>
+        create user
+      </button>
+      <CustomTable data={users} />
+      <button type="button" onClick={() => dispatch(sortName())}>
+        Sort
+      </button>
+      <CustomTable data={data} />
+    </>
+  );
+}
