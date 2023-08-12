1082a0bec393e61bfd10855598b689e8721633fe
quentinkrammer <bopatzable@gmail.com>
2023-08-12 18:20:22 +0200

    add debounced name filter

--- app1/src/components/LargeTable.tsx
+++ app1/src/components/LargeTable.tsx
@@ -1,6 +1,7 @@
+import { useDebouncedValue } from "@mantine/hooks";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useCallback, useMemo, useState } from "react";
+import { ComponentProps, useCallback, useMemo, useState } from "react";
 import { CustomTable } from "shared";
 import { z } from "zod";
 
@@ -22,8 +23,6 @@ const dataSchema = z.array(
 type Data = z.infer<typeof dataSchema>;
 
 export const LargeTable: React.FC<object> = () => {
-  const [isSortedAsc, setIsSortedAsc] = useState(true);
-
   const { isLoading, error, data } = useQuery({
     queryKey: ["repoData"],
     queryFn: () =>
@@ -34,21 +33,11 @@ export const LargeTable: React.FC<object> = () => {
       }),
   });
 
-  const onSort = useCallback(() => {
-    setIsSortedAsc((old) => !old);
-  }, []);
-
-  const sorted = useMemo(() => {
-    return (
-      data?.sort(function (a, b) {
-        const nameA = a.name.toLowerCase();
-        const nameB = b.name.toLowerCase();
-        if (nameA < nameB) return isSortedAsc ? -1 : 1;
-        if (nameA > nameB) return isSortedAsc ? 1 : -1;
-        return 0;
-      }) ?? []
-    );
-  }, [data, isSortedAsc]);
+  const {
+    callbacks: { onFilter, onSort },
+    data: filteredAndSortedData,
+    filter,
+  } = useFilteredAndSortedData(data ?? []);
 
   if (isLoading) {
     return "Loading...";
@@ -57,13 +46,45 @@ export const LargeTable: React.FC<object> = () => {
     console.log(error);
     return "Error :(";
   }
-
   return (
     <>
       <button type="button" onClick={onSort}>
         sort
       </button>
-      <CustomTable data={sorted} />
+      <input value={filter} onChange={onFilter} />
+      <CustomTable data={filteredAndSortedData} />
     </>
   );
 };
+
+function useFilteredAndSortedData(data: Data) {
+  const [isSortedAsc, setIsSortedAsc] = useState(true);
+  const [filter, setFilter] = useState("");
+  const [debouncedFilter] = useDebouncedValue(filter, 200);
+
+  const onSort = useCallback(() => {
+    setIsSortedAsc((old) => !old);
+  }, [setIsSortedAsc]);
+
+  const onFilter = useCallback<
+    NonNullable<ComponentProps<"input">["onChange"]>
+  >((e) => setFilter(e.target.value), [setFilter]);
+
+  const filtered = useMemo(() => {
+    return data?.filter(({ name }) => name.includes(debouncedFilter));
+  }, [data, debouncedFilter]);
+
+  const sorted = useMemo(() => {
+    return (
+      filtered?.sort(function (a, b) {
+        const nameA = a.name.toLowerCase();
+        const nameB = b.name.toLowerCase();
+        if (nameA < nameB) return isSortedAsc ? -1 : 1;
+        if (nameA > nameB) return isSortedAsc ? 1 : -1;
+        return 0;
+      }) ?? []
+    );
+  }, [filtered, isSortedAsc]);
+
+  return { callbacks: { onSort, onFilter }, data: sorted, filter };
+}
