dc36b8ae9bb8150bbadb026bb077632304a27a23
quentinkrammer <bopatzable@gmail.com>
2023-08-15 15:22:03 +0200

    Revert "custom table accepts ReactNodes"
    This reverts commit 57d7fb1cee582ea06a3688310710590e3d3354f4.

--- shared/src/components/CustomTable.tsx
+++ shared/src/components/CustomTable.tsx
@@ -1,19 +1,24 @@
 import { Column } from "primereact/column";
 import { DataTable } from "primereact/datatable";
-import { ReactNode } from "react";
 
 export type CustomTableProps<TObj extends object> = {
   data: Array<TObj>;
 };
-export function CustomTable<TObj extends { [index: string]: ReactNode }>({
+export function CustomTable<TObj extends { [index: string]: number | string }>({
   data,
 }: CustomTableProps<TObj>) {
-  const orderedColumnValues = Object.values(data[0] ?? []);
+  const orderedColumnKeys = Object.keys(data[0] ?? []);
 
   return (
     <DataTable value={data} paginator rows={5}>
-      {orderedColumnValues.map((columnValue, index) => (
-        <Column key={index} body={columnValue} />
+      {orderedColumnKeys.map((column) => (
+        <Column
+          key={column}
+          body={(cellData) => {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+            return cellData[column];
+          }}
+        />
       ))}
     </DataTable>
   );
